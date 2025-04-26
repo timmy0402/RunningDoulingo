19decabbe9058420d258f051286953e01a3341a8
DulguunD <delgerbatdulguun@gmail.com>
2025-04-26 12:52:22 -0700

    Deleting extra

--- step-up/RunningDuolingo/pages/HomeScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function HomeScreen() {
-    return (
-        <View>
-         <Text>Welcome to the Home Page! </Text>
-         </View>
-        );
-    }
