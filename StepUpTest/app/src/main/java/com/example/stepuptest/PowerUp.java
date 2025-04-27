a8eb9ef1676c04f9bb9986beeb0f0f02d0313ac0
timmy0402 <minhnhathb04@gmail.com>
2025-04-26 17:03:16 -0700

    new screen

--- StepUpTest/app/src/main/java/com/example/stepuptest/PowerUp.java
+++ StepUpTest/app/src/main/java/com/example/stepuptest/PowerUp.java
@@ -1,5 +1,6 @@
-package com.example.stepuptest.ui;
+package com.example.stepuptest;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,7 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.stepuptest.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PowerUp extends AppCompatActivity {
 
@@ -21,6 +22,30 @@ public class PowerUp extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.nav_view);
+        bottomNavigationView.setSelectedItemId(R.id.power_up);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.navigation_home) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(0, 0);
+                return true;
+            } else if (id == R.id.power_up) {
+                return true;
+            } else if (id == R.id.login) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                overridePendingTransition(0, 0);
+                return true;
+            } else if (id == R.id.rank) {
+                startActivity(new Intent(getApplicationContext(), Rank.class));
+                overridePendingTransition(0, 0);
+                return true;
+            }
+            return false;
         });
     }
 }
